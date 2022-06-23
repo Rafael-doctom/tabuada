479da973ef414ada010dfc347dc414ec8a6525de
Rafael-doctom <dasilva.rafaelpt@gmail.com>
2022-06-23 18:29:59 -0300

    feat: new style added

--- script.js
+++ script.js
@@ -1,20 +1,13 @@
 
 function gerarTabuada() {
+
     const numeroTabuada = document.getElementById("numeroTabuada").value;
     const resultado = document.getElementById("resultado");
 
-    // var n = 0
-    var total = ' ';
-    for (var n = 0; n <= 10; n++){
+    var total = '';
+    for (var n = 0; n <= 10; n++) {
         total += numeroTabuada + " x " + n + " = " +
-        numeroTabuada * n + "<br/>";
-
-        // total += `${numeroTabuada}x${n}=${total}`;
-
-        
-    resultado.innerHTML = total;
-    }
-     
-
-}
-
+            numeroTabuada * n + "<br/>";
+        resultado.innerHTML = total;
+    };
+};
